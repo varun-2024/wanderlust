386aae7e2ed6766c26fa4b7665414c6523065fa0
varun-2024 <v.talwar2024@gmail.com>
2025-04-20 22:14:27 +0100

    Remove required constraint from image field in listing schema

--- models/listing.js
+++ models/listing.js
@@ -9,7 +9,6 @@ const listingSchema = new Schema({
   description: String,
   image: {
     type: String,
-    required: true,
     set: (v) =>
       v === ""
         ? "https://images.unsplash.com/photo-1744791588287-6660d83418b3?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
