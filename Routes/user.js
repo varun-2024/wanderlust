e49320a6c845057493cc3c4c8d2a229f0490c8ec
varun-2024 <v.talwar2024@gmail.com>
2025-05-09 23:41:20 +0100

    Add user login view: create login form with validation and layout integration

--- Routes/user.js
+++ Routes/user.js
@@ -2,19 +2,43 @@
 const express = require("express");
 // Require Router
 const router = express.Router({ mergeParams: true });
-
+const asyncWrap = require("../utils/wrapAsync.js");
+const passport = require("passport");
 const User = require("../models/user.js");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
 });
-router.post("/signup", async (req, res) => {
-  let { username, email, password } = req.body;
-  const newUser = new User({ email, username });
-  const registeredUser = await User.register(newUser, password);
-  console.log(registeredUser);
-  req.flash("success", "User was registered Successfully");
-  res.redirect("/listings");
+router.post(
+  "/signup",
+  asyncWrap(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ email, username });
+      const registeredUser = await User.register(newUser, password);
+      console.log(registeredUser);
+      req.flash("success", "User was registered Successfully");
+      res.redirect("/listings");
+    } catch (err) {
+      req.flash("error", err.message);
+      res.redirect("/signup");
+    }
+  })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
 });
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  asyncWrap(async (req, res) => {
+    res.send("Welcome, you ar logged in!");
+  })
+);
 
 module.exports = router;
